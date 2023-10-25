74ea3423f8c2c3e26e546a8bf015f85ef2c0b06f
ActuallyRuben <118456155+ActuallyRuben@users.noreply.github.com>
2023-10-25 19:15:03 +0200

    Initial commit

---
+++ overload/src/lib.rs
@@ -0,0 +1,7 @@
+#[cfg(test)]
+mod test;
+
+fn returns_one() -> u32 {
+    println!("Hello, world!");
+    1
+}
---
+++ overload/src/test.rs
@@ -0,0 +1,6 @@
+use crate::returns_one;
+
+#[test]
+fn returns_one_returns_one() {
+    assert_eq!(returns_one(), 1);
+}
