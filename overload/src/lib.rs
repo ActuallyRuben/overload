2fe9abafb57d1fea780e584e09cff26c010a59bd
ActuallyRuben <118456155+ActuallyRuben@users.noreply.github.com>
2023-10-26 20:59:57 +0200

    Mess around with tests a bit

--- overload/src/lib.rs
+++ overload/src/lib.rs
@@ -1,6 +1,12 @@
 #![feature(fn_traits, unboxed_closures)]
+#![feature(tuple_trait)]
 
+use std::marker::Tuple;
 pub use overload_macros::overload;
 
 #[cfg(test)]
 mod test;
+
+trait CheekyFnOnce<R, Ts: Tuple>: FnOnce<Ts, Output=R> {
+    fn call(&self, args: Ts) -> R;
+}
--- overload/src/test.rs
+++ overload/src/test.rs
@@ -1,8 +1,57 @@
-use crate::returns_one;
+use std::ops::{Add, Mul};
+
+use crate::overload;
+
+overload! {
+    pub fn add {
+        (a: i32, b: i32) -> i32 {
+            let result = a + b;
+            result
+        }
+
+        ((a, b): (i32, i32), (c, d): (i32, i32)) -> (i32, i32) {
+            (a + c, b + d)
+        }
+
+        (a: &str, b: &str) -> Option<i32> {
+            Some(a.parse::<i32>().ok()? + b.parse::<i32>().ok()?)
+        }
+
+        <I: Iterator<Item=T>, T: Add<T, Output=T> + Default>(vals: I) -> T {
+            vals.fold(Default::default(), |x, y| {
+                x + y
+            })
+        }
+
+        () -> i32 {
+            0
+        }
+    }
+}
+
+overload! {
+    fn double {
+        <T: Mul<u32>>(value: T) -> T::Output {
+            value * 2
+        }
+
+        (a: &str, _: ()) -> String {
+            format!("{}{}", a, a)
+        }
+    }
+}
 
 #[test]
-fn returns_one_returns_one() {
-    assert_eq!(returns_one(-1, 2), 1);
-    assert_eq!(returns_one(1), 1);
-    assert_eq!(returns_one(), 1);
+fn addition() {
+    assert_eq!(add(6, -7), -1);
+    assert_eq!(add((4, 5), (6, 7)), (10, 12));
+    assert_eq!(add("6", "-7"), Some(-1));
+    assert_eq!(add([5, 6, 7, 8].into_iter()), 5 + 6 + 7 + 8);
+    assert_eq!(add(), 0);
+}
+
+#[test]
+fn doubling() {
+    assert_eq!(double(5), 10);
+    assert_eq!(double("hello", ()), "hellohello");
 }
